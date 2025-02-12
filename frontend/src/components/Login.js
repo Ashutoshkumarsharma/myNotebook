aac9b16025f525de348d5727df6c9453cfca80fd
Ashutosh Kumar Sharma <163313586+Ashutoshkumarsharma@users.noreply.github.com>
2025-02-13 01:55:41 +0530

    Updated project with new changes

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -17,7 +17,7 @@ const Login = (props) => {
       body: JSON.stringify({ email: credentials.email, password: credentials.password }),
     });
     const json = await response.json();
-    // console.log(json);
+    console.log(json);
     if (json.success) {
       localStorage.setItem('token', json.authtoken);
       props.showAlert('Logged in Successfully ', 'success');
